7dc99ae02f5068d74a713dfff25b614c1f54005e
Sebastian Hahn <Wastel.Wastelcompany@gmail.com>
2026-02-21 18:26:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9340678048-ced33",
+  "appId": "1:682561475621:web:5b2f46fa2ca2b0e19a1bcc",
+  "apiKey": "AIzaSyAw_7q2ai7r047wo3R3nWFZP0fXnl2rVw8",
+  "authDomain": "studio-9340678048-ced33.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "682561475621"
+};
